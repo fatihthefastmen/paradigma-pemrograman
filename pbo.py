91e3f0f564c6f5231220e7bebc3995cd29abe403
fatihthefastmen <122343971+fatihthefastmen@users.noreply.github.com>
2023-02-21 14:42:02 +0700

    Add files via upload

---
+++ pbo.py
@@ -0,0 +1,15 @@
+class segitiga:
+  def __int__(self, alas, tinggi):
+    self.alas = alas
+    self.tinggi = tinggi
+
+def get_luas(self):
+    return 0.5 * self.alas * self.tinggi
+
+segitiga1 = segitiga(5, 10)
+segitiga2 = segitiga(7, 15)
+segitiga3 = segitiga(9, 20)
+
+print('luas segitiga : ', segitiga.get_luas())
+print('luas segitiga : ', segitiga.get_luas())
+print('luas segitiga : ', segitiga.get_luas())
